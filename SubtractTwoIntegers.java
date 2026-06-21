be5c08250c912cd8ec47b7c1f148fc63090b1981
madhumitanawkar05-crypto <madhumitanawkar05@gmail.com>
2026-06-21 15:14:32 +0530

    Add files via upload

---
+++ SubtractTwoIntegers.java
@@ -0,0 +1,9 @@
+public class SubtractTwoIntegers {
+    static int subtract(int a, int b) {
+        return a - b;
+    }
+
+    public static void main(String[] args) {
+        System.out.println("Difference = " + subtract(20, 10));
+    }
+}
